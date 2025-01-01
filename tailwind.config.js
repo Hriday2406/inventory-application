7c7af6007c70303ff23e97171bd5476460c030fb
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2025-01-02 02:04:06 +0530

    Tailwind setup and styling complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ["./views/**/*.ejs", "./public/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['"Roboto"', "sans-serif"],
+        mono: ['"Roboto Mono"', "serif"],
+        script: ['"Dancing Script"', "serif"],
+      },
+      colors: {
+        bg: "#0F0F0F",
+        accent: "#AE9B84",
+        secondary: "#1F1F1F",
+      },
+    },
   },
   plugins: [],
 };
-
