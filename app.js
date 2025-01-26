c0cd32531f2873d60bd16f47b6306928097c887c
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2025-01-27 02:36:45 +0530

    Connection Refused Hot Fix

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
-const indexRouter = require("./routes/indexRouter");
 require("dotenv").config();
+
+const indexRouter = require("./routes/indexRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
